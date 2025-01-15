c4b8a42275ec7e06d25b8baf700392e39aa1a7ce
JesusM <jesusmunoz0402@gmail.com>
2025-01-15 15:52:12 -0800

    update class 2

--- posts/urls.py
+++ posts/urls.py
@@ -4,6 +4,7 @@ from posts import views
 urlpatterns = [
     path("", views.PostListView.as_view(), name="list"),
     path("draft/", views.DraftPostListView.as_view(), name="drafts"),
+    path("archived/", views.ArchivedPostListView.as_view(), name="archive"),
     path("<int:pk>/", views.PostDetailView.as_view(), name="post"),
     path("new/", views.PostCreateView.as_view(), name="new"),
     path("<int:pk>/edit/", views.PostUpdateView.as_view(), name="update"),
--- posts/views.py
+++ posts/views.py
@@ -43,10 +43,38 @@ class DraftPostListView(LoginRequiredMixin, ListView):
             .order_by("created_on").reverse()
         )
         return context
+    
+class ArchivedPostListView(LoginRequiredMixin, ListView):
+    template_name = "posts/list.html"
+    model = Post
 
-class PostDetailView(DetailView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        archive = Status.object.get(name="archived")
+        context["title"] = "Archived"
+        context["post_list"] = (
+            Post.objects
+            .filter(status=archived)
+            .order_by("created_on").reverse()
+        )
+        return context
+    
+class PostDetailView(UserPassesTestMixin, DetailView):
     template_name = "posts/detail.html"
     model = Post
+
+    def test_func(self):
+        post = self.get_object()
+        if post.status.name == "published":
+            return True
+        elif post.status.name == "draft":
+            if (self.request.user.is_authenticated and self.request.user == post.author):
+                return True
+        elif post.status.name == "archived" and self.request.user.is_authenticated:
+            return True
+        else:
+            return False
+
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     template_name = "posts/new.html"
